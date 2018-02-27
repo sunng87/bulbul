c3493705fd798445b95109386d34aa23965e3c92
Ning Sun <sunng@protonmail.com>
2018-02-27 23:40:27 +0800

    (feat) wip open and create segfile
    Signed-off-by: Ning Sun <sunng@protonmail.com>

--- src/bulbul/seg.clj
+++ src/bulbul/seg.clj
@@ -1,5 +1,7 @@
 (ns bulbul.seg
-  (:require [bulbul.protocol :as p]))
+  (:require [bulbul.protocol :as p]
+            [clojure.java.io :as io])
+  (:import [java.io RandomAccessFile]))
 
 (defrecord SegmentLog [state config])
 
@@ -20,11 +22,12 @@
   (close! [this]) )
 
 (defn segment-log-initial-state []
-  {:index 0})
+  {:index 0
+   :id 0})
 
 (defn segment-log-default-config []
   {:directory "./bulbul_log"
-   :max-entry 1E6
+   :max-entry 1000000
    :max-size (* 5 1024 1024)})
 
 (defn segment-log [config]
@@ -33,4 +36,22 @@
     (SegmentLog. state config)))
 
 (defn open-segment-file [file]
-  )
+  (let [raf (RandomAccessFile. file "rw")
+        magic-number (.readLong raf)
+        version (.readInt raf)
+        index (.readLong raf)
+        max-size (.readInt raf)
+        max-entry (.readInt raf)]
+    {:fd raf
+     :meta {:version version
+            :max-size max-size
+            :max-entry max-entry}
+     :index index}))
+
+(defn- segment-file-name [config id]
+  (str (:directory config) "/" (:name config) ".log." id))
+
+(defn create-segment-file [id index config]
+  (let [raf (RandomAccessFile. (segment-file-name config id))]
+    ;; TODO: write header
+    ))
