a144ec202025310393f07babc8501f47421086ba
Ning Sun <sunng@protonmail.com>
2018-03-01 22:50:44 +0800

    (feat) open bulbul file
    Signed-off-by: Ning Sun <sunng@protonmail.com>

--- src/bulbul/seg.clj
+++ src/bulbul/seg.clj
@@ -3,6 +3,9 @@
             [clojure.java.io :as io])
   (:import [java.io RandomAccessFile]))
 
+(def magic-number (.getBytes "BULBUL_LOG_010" "UTF-8"))
+(def version 1)
+
 (defrecord SegmentLog [state config])
 
 (extend-protocol p/LogStore
@@ -28,7 +31,8 @@
 (defn segment-log-default-config []
   {:directory "./bulbul_log"
    :max-entry 1000000
-   :max-size (* 5 1024 1024)})
+   :max-size (* 5 1024 1024)
+   :version version})
 
 (defn segment-log [config]
   (let [state (atom (segment-log-initial-state))
@@ -37,8 +41,9 @@
 
 (defn open-segment-file [file]
   (let [raf (RandomAccessFile. file "rw")
-        magic-number (.readLong raf)
+        _ (.readFully raf (byte-array (alength magic-number)))
         version (.readInt raf)
+        id (.readInt raf)
         index (.readLong raf)
         max-size (.readInt raf)
         max-entry (.readInt raf)]
@@ -46,12 +51,24 @@
      :meta {:version version
             :max-size max-size
             :max-entry max-entry}
-     :index index}))
+     :index index
+     :id id}))
 
 (defn- segment-file-name [config id]
   (str (:directory config) "/" (:name config) ".log." id))
 
 (defn create-segment-file [id index config]
-  (let [raf (RandomAccessFile. (segment-file-name config id))]
-    ;; TODO: write header
-    ))
+  (let [raf (RandomAccessFile. (segment-file-name config id) "rw")]
+    (.write raf magic-number)
+    (.writeInt raf (:version config))
+    (.writeInt raf id)
+    (.writeLong raf index)
+    (.writeInt (:max-size config))
+    (.writeInt (:max-entry config))
+
+    {:fd raf
+     :meta {:version version
+            :max-size (:max-size config)
+            :max-entry (:max-entry config)}
+     :index index
+     :id id}))
