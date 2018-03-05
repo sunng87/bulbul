0950f2ce92e07257f5f36bb370ee50385829c71e
Ning Sun <sunng@protonmail.com>
2018-03-05 23:30:52 +0800

    (feat) refactor file api to NIO
    Signed-off-by: Ning Sun <sunng@protonmail.com>

--- src/bulbul/seg.clj
+++ src/bulbul/seg.clj
@@ -1,7 +1,8 @@
 (ns bulbul.seg
   (:require [bulbul.protocol :as p]
             [clojure.java.io :as io])
-  (:import [java.io RandomAccessFile]))
+  (:import [java.io RandomAccessFile]
+           [java.nio ByteBuffer]))
 
 (def magic-number (.getBytes "BULBULLOG1" "UTF-8"))
 (def version 1)
@@ -39,35 +40,43 @@
     ))
 
 (defn open-segment-file [file]
-  (let [raf (RandomAccessFile. file "rw")
-        magic-number-array (byte-array (alength magic-number))]
-    (.readFully raf magic-number-array)
-    (when (= (seq magic-number) (seq magic-number-array))
-      (let [version (.readInt raf)
-            id (.readInt raf)
-            index (.readLong raf)
-            max-size (.readInt raf)
-            max-entry (.readInt raf)]
-        (.skipBytes raf header-retain-size)
-        {:fd raf
-         :meta {:version version
-                :max-size max-size
-                :max-entry max-entry}
-         :start-index index
-         :id id}))))
+  (let [raf (.getChannel (RandomAccessFile. file "rw"))
+        hb (ByteBuffer/allocate header-total-size)
+        header-size (.read raf hb)]
+    (if (= header-size header-total-size)
+      (let [magic-number-array (byte-array (alength magic-number))]
+        (.get hb magic-number-array)
+        (if (= (seq magic-number) (seq magic-number-array))
+          (let [version (.getInt hb)
+                id (.getInt hb)
+                index (.getLong raf)
+                max-size (.getInt raf)
+                max-entry (.getInt raf)]
+            {:fd raf
+             :meta {:version version
+                    :max-size max-size
+                    :max-entry max-entry}
+             :start-index index
+             :id id})
+          ;; not a bulbul file
+          (.close raf)))
+      (.close raf))))
 
 (defn- segment-file-name [config id]
   (str (:directory config) "/" (:name config) ".log." id))
 
 (defn create-segment-file [id index config]
-  (let [raf (RandomAccessFile. (segment-file-name config id) "rw")]
-    (.write raf magic-number)
-    (.writeInt raf (:version config))
-    (.writeInt raf id)
-    (.writeLong raf index)
-    (.writeInt (:max-size config))
-    (.writeInt (:max-entry config))
-    (.skipBytes raf header-retain-size)
+  (let [raf (.getChannel (RandomAccessFile. (segment-file-name config id) "rw"))
+        hb (ByteBuffer/allocate header-total-size)]
+    (.put hb magic-number)
+    (.putInt hb (:version config))
+    (.putInt raf id)
+    (.putLong raf index)
+    (.putInt (:max-size config))
+    (.putInt (:max-entry config))
+
+    (.write raf hb)
+
     {:fd raf
      :meta {:version version
             :max-size (:max-size config)
