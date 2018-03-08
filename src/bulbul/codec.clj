53f43b42ef84f689138e7daaa47752938bf76ff1
Ning Sun <sunng@protonmail.com>
2018-03-08 23:48:13 +0800

    (feat) added buffer extend
    Signed-off-by: Ning Sun <sunng@protonmail.com>

--- src/bulbul/codec.clj
+++ src/bulbul/codec.clj
@@ -2,6 +2,11 @@
   (:import [java.nio ByteBuffer]))
 
 ;; TODO: fix ByteBuffer based api
+(defmacro defcodec [sym encoder-fn decoder-fn]
+  `(defn ~sym [& options#]
+     {:encoder (partial ~encoder-fn options#)
+      :decoder (partial ~decoder-fn options#)}))
+
 
 ;; macro to improve codec readability
 (defmacro encoder [args & body]
@@ -10,13 +15,23 @@
 (defmacro decoder [args & body]
   `(fn ~args ~@body))
 
+(defn- available-bytes [^ByteBuffer buf]
+  (.remaining​ buf))
+
+(defn- ensure-buffer [buffer size]
+  (let [space (available-bytes buffer)]
+    (if (< space size)
+      (doto (ByteBuffer/allocate (* 2 (.limit buffer)))
+        (.put (.flip buffer)))
+      buffer)))
+
 (defmacro primitive-codec [sname size writer-fn reader-fn]
   `(defcodec ~sname
      (encoder [_# data# ^ByteBuffer buffer#]
-              (. buffer# ~writer-fn data#)
-              buffer#)
+              (doto (ensure-buffer buffer# ~size)
+                (. ~writer-fn data#)))
      (decoder [_# ^ByteBuffer buffer#]
-              (when (>= (- (.limit buffer#) (.position buffer#)) ~size)
+              (when (>= (available-bytes buffer#) ~size)
                 (. buffer# ~reader-fn)))))
 
 (primitive-codec byte 1 put get)
@@ -26,37 +41,39 @@
 (primitive-codec float 4 putFloat getFloat)
 (primitive-codec double 8 putDouble getDouble)
 
-(defn- find-delimiter [^ByteBuf src ^bytes delim]
-  (loop [sindex (.readerIndex src) dindex 0]
-    (if (= sindex (.writerIndex src))
+(defn- find-delimiter [^ByteBuffer src ^bytes delim]
+  (loop [sindex (.position src) dindex 0]
+    (if (= sindex (.limit src))
       -1
-      (if (= ^Byte (.getByte src sindex) ^Byte (aget delim dindex))
+      (if (= ^Byte (.get src sindex) ^Byte (aget delim dindex))
         (if (= dindex (- (alength delim) 1))
-          (+ (- sindex (.readerIndex src)) 1)
+          (+ (- sindex (.position src)) 1)
           (recur (inc sindex) (inc dindex)))
         (recur (inc sindex) 0)))))
 
 (defcodec string
-  (encoder [options ^String data ^ByteBuf buffer]
+  (encoder [options ^String data ^ByteBuffer buffer]
            (let [{:keys [prefix encoding delimiter]} options
                  encoding (name encoding)
-                 bytes (.getBytes (or data "") encoding)]
+                 bytes (.getBytes ^String (or data "") ^String encoding)]
              (cond
                (and (nil? delimiter) (nil? prefix))
                (throw (IllegalArgumentException. "Neither :delimiter nor :prefix provided for string codec"))
                ;; length prefix string
                (nil? delimiter)
-               (do
-                 ((:encoder prefix) (alength bytes) buffer)
-                 (.writeBytes buffer ^bytes bytes))
+               (let [length (alength bytes)]
+                 (as-> buffer $buf
+                   ((:encoder prefix) length $buf)
+                   (.put (ensure-buffer $buf length) ^bytes bytes)))
                ;; delimiter based string
                (nil? prefix)
-               (do
-                 (.writeBytes buffer ^bytes bytes)
-                 (.writeBytes buffer ^bytes
-                              (.getBytes ^String delimiter encoding)))))
+               (let [delimiter-bytes (.getBytes ^String delimiter encoding)]
+                 (as-> buffer $buf
+                   (.put (ensure-buffer $buf (alength bytes)) ^bytes bytes)
+                   (.put (ensure-buffer $buf (alength delimiter-bytes))
+                         ^bytes delimiter-bytes)))))
            buffer)
-  (decoder [options ^ByteBuf buffer]
+  (decoder [options ^ByteBuffer buffer]
            (let [{:keys [prefix encoding delimiter]} options
                  encoding (name encoding)]
              (cond
@@ -66,9 +83,9 @@
                (nil? delimiter)
                (do
                  (when-let [byte-length ((:decoder prefix) buffer)]
-                   (when-not (> byte-length (.readableBytes buffer))
+                   (when-not (> byte-length (available-bytes buffer))
                      (let [bytes (byte-array byte-length)]
-                       (.readBytes buffer ^bytes bytes)
+                       (.get buffer ^bytes bytes)
                        (String. bytes encoding)))))
 
                ;; delimiter based string
@@ -79,29 +96,30 @@
                      slength (- dlength dmlength)]
                  (when (> slength 0)
                    (let [sbytes (byte-array slength)]
-                     (.readBytes buffer ^bytes sbytes)
+                     (.get buffer ^bytes sbytes)
                      ;; move readerIndex
-                     (.readerIndex buffer (+ dmlength (.readerIndex buffer)))
+                     (.position buffer (+ dmlength (.position buffer)))
                      (String. sbytes encoding))))))))
 
 (defcodec byte-block
-  (encoder [options ^ByteBuf data ^ByteBuf buffer]
+  (encoder [options ^bytes data ^ByteBuffer buffer]
            (let [{prefix :prefix encode-length-fn :encode-length-fn} options
                  encode-length-fn (or encode-length-fn identity)
-                 byte-length (if (nil? data) 0 (.readableBytes data))
+                 byte-length (if (nil? data) 0 (alength data))
                  encoded-length (encode-length-fn byte-length)]
-             ((:encoder prefix) encoded-length buffer)
-             (when-not (nil? data)
-               (.writeBytes buffer ^ByteBuf data)
-               (.release ^ByteBuf data))
-             buffer))
-  (decoder [options ^ByteBuf buffer]
+             (as-> buffer $buf
+               ((:encoder prefix) encoded-length buffer)
+               (if (some? data)
+                 (.put (ensure-buffer $buf byte-length) data)
+                 $buf))))
+  (decoder [options ^ByteBuffer buffer]
            (let [{prefix :prefix decode-length-fn :decode-length-fn} options
                  decode-length-fn (or decode-length-fn identity)
                  byte-length (decode-length-fn ((:decoder prefix) buffer))]
              (when-not (or (nil? byte-length)
-                           (> byte-length (.readableBytes buffer)))
-               (.readRetainedSlice buffer byte-length)))))
+                           (> byte-length (available-bytes buffer)))
+               (let [result-bytes (byte-array byte-length)]
+                 (.get buffer result-bytes))))))
 
 (def ^{:private true} reversed-map
   (memoize
@@ -109,20 +127,18 @@
      (apply hash-map (mapcat #(vector (val %) (key %)) m)))))
 
 (defcodec enum
-  (encoder [options data ^ByteBuf buffer]
+  (encoder [options data ^ByteBuffer buffer]
            (let [[codec mapping] options
-                 mapping (if (util/derefable? mapping) @mapping mapping)
                  value (get mapping data)]
              ((:encoder codec) value buffer)))
-  (decoder [options ^ByteBuf buffer]
+  (decoder [options ^ByteBuffer buffer]
            (let [[codec mapping] options
-                 mapping (if (util/derefable? mapping) @mapping mapping)
                  mapping (reversed-map mapping)
                  value ((:decoder codec) buffer)]
              (get mapping value))))
 
 (defcodec header
-  (encoder [options data ^ByteBuf buffer]
+  (encoder [options data ^ByteBuffer buffer]
            (let [[enumer children] options
                  head (first data)
                  body (second data)
@@ -130,63 +146,56 @@
              ((:encoder enumer) head buffer)
              ((:encoder body-codec) body buffer)
              buffer))
-  (decoder [options ^ByteBuf buffer]
+  (decoder [options ^ByteBuffer buffer]
            (let [[enumer children] options
                  head ((:decoder enumer) buffer)
                  body (and head ;; body is nil if head is nil
                            ((:decoder (get children head)) buffer))]
-             (if-not (nil? body)
-               [head body]
-               (do
-                 (try-release body)
-                 nil)))))
+             (when-not (nil? body)
+               [head body]))))
 
 (defcodec frame
-  (encoder [options data ^ByteBuf buffer]
+  (encoder [options data ^ByteBuffer buffer]
            (let [codecs options]
              (dorun (map #((:encoder %1) %2 buffer) codecs data))
              buffer))
-  (decoder [options ^ByteBuf buffer]
+  (decoder [options ^ByteBuffer buffer]
            (let [codecs options]
              (loop [c codecs r []]
                (if (empty? c)
                  r
-                 (if-let [r0 ((:decoder (first c)) buffer)]
-                   (recur (rest c) (conj r r0))
-                   (do
-                     (try-release r)
-                     nil)))))))
+                 (when-let [r0 ((:decoder (first c)) buffer)]
+                   (recur (rest c) (conj r r0))))))))
 
 (defcodec counted
-  (encoder [options data ^ByteBuf buffer]
+  (encoder [options data ^ByteBuffer buffer]
            (let [length (count data)
                  {length-codec :prefix body-codec :body} options]
              ((:encoder length-codec) length buffer)
              (doseq [frm data]
                ((:encoder body-codec) frm buffer))
              buffer))
-  (decoder [options ^ByteBuf buffer]
+  (decoder [options ^ByteBuffer buffer]
            (let [{length-codec :prefix body-codec :body} options
                  length ((:decoder length-codec) buffer)]
              (when length
                (loop [idx 0 results []]
                  (if (== idx length)
                    results
-                   (if-let [data ((:decoder body-codec) buffer)]
-                     (recur (inc idx) (conj results data))
-                     (do
-                       (try-release results)
-                       nil))))))))
+                   (when-let [data ((:decoder body-codec) buffer)]
+                     (recur (inc idx) (conj results data)))))))))
 
 (defcodec const
-  (encoder [options data ^ByteBuf buffer]
+  (encoder [options data ^ByteBuffer buffer]
            buffer)
-  (decoder [options ^ByteBuf buffer]
+  (decoder [options ^ByteBuffer buffer]
            (first options)))
 
-(defn encode*
-  ([codec data ^ByteBuf buffer]
-   ((:encoder codec) data buffer)))
+(defn encode
+  ([codec data ^ByteBuffer buffer]
+   ((:encoder codec) data buffer))
+  ([codec data]
+   (encode codec data (ByteBuffer/allocate 256))))
 
-(defn decode* [codec ^ByteBuf buffer]
+(defn decode [codec ^ByteBuffer buffer]
   ((:decoder codec) buffer))
