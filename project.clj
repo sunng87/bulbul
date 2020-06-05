6095d32604512fc964e9371583b2129b5a05eaca
Ning Sun <sunng@protonmail.com>
2020-06-05 23:01:30 +0800

    (feat) deps up

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/sunng87/bulbul"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/data.avl "0.0.17"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.avl "0.1.0"]]
   :deploy-repositories {"releases" :clojars})
